9e811dfccc6d906f73048a79d3748f4670ff3eb0
Josh Bavari <josh@raisemore.com>
2013-11-06 20:01:26 -0600

    Moved code from controller to contact model

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -6,48 +6,20 @@ class Api::ContactsController < ApplicationController
 		p 'checking the key'
 		@token = session[:token]
 		if(@token.nil?)
-			p ' no key was found.'
 			render :json  => { success: :false, message: 'You must login first. ' }.to_json
 		end
 	end
 
 	def auth
 		provider = params[:provider]
-		auth = request.env["omniauth.auth"]
-		#raise auth.to_yaml
-
-		token = auth["credentials"]["token"]
-
-		p 'token: ' + token.to_s
-
-		session[:token] = token
-
+		session[:token] = request.env["omniauth.auth"]["credentials"]["token"]
 		render :json => true
-
 	end
 
 	def search
 		search_term = params[:name]
-
-		users_array = Array.new
-
-		users = FbGraph::User.search(search_term, :access_token => @token)
-
-		users.each do |user|
-			item = { name: user.name, fbid: user.identifier, email: user.email, picurl: "https://graph.facebook.com/#{user.identifier}/picture" }
-
-			users_array << item
-		end
-
-		render :json => { success: :true, payload: users_array }
+		users = Contact.search(search_term, @token)
+		render :json => { success: :true, payload: users }
 	end
 
 end
-# Rector	Joshua	Strategic Team BJJ
-# Bavari	Josh	American Elite MMA
-# Wolf	Justin	Triton Fight Center Bixby
-# Luksic	Michael	Guetho Texas
-# Coppock	Michael	Lovato BJJ
-# Irwin	Sebastian	Academy of Martial Arts
-# Jordan	Virgil	Red River BJJ
-# Young	William	Lovato BJJ
