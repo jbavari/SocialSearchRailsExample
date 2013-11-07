d504b9cfe9f8559908b49ca4aaca3102e6436950
Josh Bavari <josh@raisemore.com>
2013-11-06 18:38:12 -0600

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SocialSearchRailsExample::Application.config.secret_key_base = '7a3d52d4983a9ab5c2076e792a2ba44fa0176c0ac6941fc0dfa173abf0b21141a0cf62f9d238ec5ca566d4f414e0a48245f0a39fd5ef1f7b320c577e324cd345'
